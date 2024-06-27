3b1d0f02563345a4def7e6ce83e986fd41273d8b
Gustavo Vasquez <gustavo.vasquez@corhuila.edu.co>
2024-06-26 20:46:22 -0500

    Agrupar exportaciones

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // import { printObject } from "./generics/generics";
 
 import { genericFunction, genericFunctionArrow } from "./generics/generics";
+import { Hero, Villain } from "./interfaces";
+
 
 
 // printObject('Hola');
@@ -8,6 +10,14 @@ import { genericFunction, genericFunctionArrow } from "./generics/generics";
 // printObject( new Date());
 // printObject( {a: 1, b: 'Hola', c: true});
 
-console.log(genericFunction(3.1416).toFixed(2));
-console.log(genericFunction('Hola mundo').toUpperCase());
-console.log(genericFunctionArrow('Hola mundo2').toUpperCase());
+// console.log(genericFunction(3.1416).toFixed(2));
+// console.log(genericFunction('Hola mundo').toUpperCase());
+// console.log(genericFunctionArrow('Hola mundo2').toUpperCase());
+
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130,
+}
+
+console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
