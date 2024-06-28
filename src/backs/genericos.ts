6c83407ea16d68fa0a7742c40bdfcff8775118cb
Gustavo Vasquez <gustavo.vasquez@corhuila.edu.co>
2024-06-27 20:17:20 -0500

    Fin de la seccion

---
+++ src/backs/genericos.ts
@@ -0,0 +1,21 @@
+// import { printObject } from "./generics/generics";
+
+import { genericFunction, genericFunctionArrow } from "../generics/generics";
+import { Hero, Villain } from "../interfaces";
+
+
+
+// printObject('Hola');
+// printObject(123);
+// printObject( new Date());
+// printObject( {a: 1, b: 'Hola', c: true});
+
+// console.log(genericFunction(3.1416).toFixed(2));
+// console.log(genericFunction('Hola mundo').toUpperCase());
+// console.log(genericFunctionArrow('Hola mundo2').toUpperCase());
+
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-// import { printObject } from "./generics/generics";
-
-import { genericFunction, genericFunctionArrow } from "./generics/generics";
-import { Hero, Villain } from "./interfaces";
+import { getPokemon } from "./generics/get-pokemon";
 
 
-
-// printObject('Hola');
-// printObject(123);
-// printObject( new Date());
-// printObject( {a: 1, b: 'Hola', c: true});
-
-// console.log(genericFunction(3.1416).toFixed(2));
-// console.log(genericFunction('Hola mundo').toUpperCase());
-// console.log(genericFunctionArrow('Hola mundo2').toUpperCase());
-
-const deadpool = {
-    name: 'Deadpool',
-    realName: 'Wade Winston Wilson',
-    dangerLevel: 130,
-}
-
-console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
+getPokemon(4)
+    .then(pokemon => console.log(pokemon.sprites.back_default))
+    .catch(error => console.error(error))
+    .finally(() => console.log('Fin de getPokemon'))
